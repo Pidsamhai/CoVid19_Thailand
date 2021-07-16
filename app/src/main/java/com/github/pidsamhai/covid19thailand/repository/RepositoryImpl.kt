342b01d07dff4b42d95d118319e8255feac7c90e
mengxyz <meng348@gmail.com>
2021-07-16 13:10:56 +0700

    [changed] update AboutPage add ability to clear data base
    - clean repository replace all dao with data base
    - update should fetch if data is null

--- app/src/main/java/com/github/pidsamhai/covid19thailand/repository/RepositoryImpl.kt
+++ app/src/main/java/com/github/pidsamhai/covid19thailand/repository/RepositoryImpl.kt
@@ -3,10 +3,8 @@ package com.github.pidsamhai.covid19thailand.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import com.github.pidsamhai.covid19thailand.Result
+import com.github.pidsamhai.covid19thailand.db.CoVid19Database
 import com.github.pidsamhai.covid19thailand.db.LastFetch
-import com.github.pidsamhai.covid19thailand.db.dao.RapidDao
-import com.github.pidsamhai.covid19thailand.db.dao.TimeLineDao
-import com.github.pidsamhai.covid19thailand.db.dao.TodayDao
 import com.github.pidsamhai.covid19thailand.network.ApiResponse
 import com.github.pidsamhai.covid19thailand.network.api.CoVid19RapidApiServices
 import com.github.pidsamhai.covid19thailand.network.api.Covid19ApiServices
@@ -23,9 +21,7 @@ import timber.log.Timber
 class RepositoryImpl(
     private val covid19ApiServices: Covid19ApiServices,
     private val rapidApiServices: CoVid19RapidApiServices,
-    private val todayDao: TodayDao,
-    private val rapidDao: RapidDao,
-    private val timeLineDao: TimeLineDao,
+    private val database: CoVid19Database,
     private val lastFetch: LastFetch
 ) : Repository {
     override suspend fun getToday(): ApiResponse<Today> {
@@ -51,10 +47,10 @@ class RepositoryImpl(
     }
 
     override fun getCountryLiveData(): LiveData<Result<List<String>>> = networkBoundResource(
-        loadFromDb = { withContext(Dispatchers.IO) { rapidDao.getCountries() } },
+        loadFromDb = { withContext(Dispatchers.IO) { database.rapidDao.getCountries() } },
         saveCallResult = {
             withContext(Dispatchers.IO) {
-                rapidDao.upSertStatics(it.getStaticDatas())
+                database.rapidDao.upSertStatics(it.getStaticDatas())
             }
         },
         shouldFetch = { true },
@@ -69,10 +65,10 @@ class RepositoryImpl(
     ).asLiveData()
 
     override fun getStatic(country: String): LiveData<Result<Static>> = networkBoundResource(
-        loadFromDb = { withContext(Dispatchers.IO) { rapidDao.getStaticX(country) } },
+        loadFromDb = { withContext(Dispatchers.IO) { database.rapidDao.getStaticX(country) } },
         saveCallResult = {
             withContext(Dispatchers.IO) {
-                rapidDao.upSert(it.apply {
+                database.rapidDao.upSert(it.apply {
                     it.pk = country
                 })
             }
@@ -89,9 +85,9 @@ class RepositoryImpl(
     ).asLiveData()
 
     override fun getTodayLiveData(): Flow<Result<Today>> = networkBoundResource(
-        loadFromDb = { withContext(Dispatchers.IO) { todayDao.hashData() } },
-        saveCallResult = { withContext(Dispatchers.IO) { todayDao.upSert(it) } },
-        shouldFetch = { lastFetch.shouldFetchToday },
+        loadFromDb = { withContext(Dispatchers.IO) { database.todayDao.hashData() } },
+        saveCallResult = { withContext(Dispatchers.IO) { database.todayDao.upSert(it) } },
+        shouldFetch = { lastFetch.shouldFetchToday || it == null },
         createCall = {
             try {
                 ApiResponse.Success(covid19ApiServices.getToDay()).also {
@@ -105,9 +101,9 @@ class RepositoryImpl(
     )
 
     override fun getTimeLineLiveData(): LiveData<Result<TimeLine>> = networkBoundResource(
-        loadFromDb = { withContext(Dispatchers.IO) { timeLineDao.getTimeLine() } },
-        saveCallResult = { withContext(Dispatchers.IO) { timeLineDao.upSert(it) } },
-        shouldFetch = { lastFetch.shouldFetchTimeLine },
+        loadFromDb = { withContext(Dispatchers.IO) { database.timeLineDao.getTimeLine() } },
+        saveCallResult = { withContext(Dispatchers.IO) { database.timeLineDao.upSert(it) } },
+        shouldFetch = { lastFetch.shouldFetchTimeLine || it == null },
         createCall = {
             try {
                 ApiResponse.Success(covid19ApiServices.getTimeline()).also {
@@ -121,8 +117,8 @@ class RepositoryImpl(
     ).asLiveData()
 
     override fun getTimeLineDataLiveData(): LiveData<Result<List<Data>>> = networkBoundResource(
-        loadFromDb = { withContext(Dispatchers.IO) { timeLineDao.getDatas() } },
-        saveCallResult = { withContext(Dispatchers.IO) { timeLineDao.upSertDatas(it) } },
+        loadFromDb = { withContext(Dispatchers.IO) { database.timeLineDao.getDatas() } },
+        saveCallResult = { withContext(Dispatchers.IO) { database.timeLineDao.upSertDatas(it) } },
         shouldFetch = { true },
         createCall = {
             try {
@@ -133,4 +129,8 @@ class RepositoryImpl(
             }
         }
     ).asLiveData()
+
+    override fun clearDataBase() {
+        database.clearAllTables()
+    }
 }
